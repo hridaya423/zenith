2d52dbd4b3cc16eea766b450b0320e1180a661c2
Hridya Agrawal <hridayahoney@gmail.com>
2025-01-03 19:13:22 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    UNSPLASH_API_KEY: '',
+    SEARCH_ENGINES: {
+        google: 'https://www.google.com/search?q=',
+        bing: 'https://www.bing.com/search?q=',
+        duckduckgo: 'https://duckduckgo.com/?q='
+    },
+};
